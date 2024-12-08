a4a7f302867dc2118d8c31c6d0ab6dd2f52a4315
Naoya Furudono <naoyafurudono@gmail.com>
2024-12-09 00:35:59 +0900

    modify error handling

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
 
+	"github.com/naoyafurudono/clio-go"
 	"github.com/naoyafurudono/proto-cli/gen/greet/v1/greetv1clio"
 	"github.com/naoyafurudono/proto-cli/service"
 )
@@ -13,7 +15,13 @@ import (
 func main() {
 	greetCmd := greetv1clio.NewGreetServiceCommand(context.Background(), &service.GreetServer{}, os.Stdout)
 	if err := greetCmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		if errors.Is(err, clio.CLIFailed) {
+			panic(err)
+		} else if errors.Is(err, clio.RPCFailed) {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		} else {
+			panic("never")
+		}
 	}
 }
