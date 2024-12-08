f720be19c5857928a551b4987e387efb9ebcd627
Naoya Furudono <naoyafurudono@gmail.com>
2024-12-08 19:16:59 +0900

    generate by clio

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,13 +5,13 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/naoyafurudono/proto-cli/my_gen"
+	"github.com/naoyafurudono/proto-cli/gen/greet/v1/greetv1clio"
 	"github.com/naoyafurudono/proto-cli/service"
 )
 
 // The entry point (what you implement)
 func main() {
-	var greetCmd = my_gen.NewGreetCommand(context.Background(), &service.GreetServer{})
+	var greetCmd = greetv1clio.NewGreetCommand(context.Background(), &service.GreetServer{})
 	if err := greetCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
--- go.mod
+++ go.mod
@@ -5,11 +5,12 @@ go 1.22.6
 require (
 	connectrpc.com/connect v1.17.0
 	github.com/spf13/cobra v1.8.1
-	google.golang.org/protobuf v1.34.2
+	google.golang.org/protobuf v1.35.2
 )
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/naoyafurudono/clio-go v0.0.0-20241208094501-3e8c82bfdb5a // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/net v0.32.0 // indirect
 )
